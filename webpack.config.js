af1ac1ec11ee41212e0938ebd68f1a1961df51fe
Nazar Kosiv <nazarkosiv94@gmail.com>
2019-08-08 10:16:22 +0300

    initial commint

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/scripts', 'main.ts'),
+    },
+    mode: 'development',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './build')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: true,
+        port: 9000,
+        index: 'index.html'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                use: 'ts-loader',
+                exclude: /node_modules/,
+                test: /\.tsx?$/
+            },
+            {
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: path.resolve(__dirname)
+                            }
+                        }
+                    },
+                ],
+                test: /\.css$/
+            },
+            {
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: path.resolve(__dirname)
+                            }
+                        }
+                    },
+                    'sass-loader'],
+                test: /\.(scss|sass)$/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, './index.html')
+        })
+    ]
+};
